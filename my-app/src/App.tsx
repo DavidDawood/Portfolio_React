e8ab48f1f2ec50954db0b8e896211fbd55cae211
David D <daviddawood6161@hotmail.com>
2022-09-21 15:45:54 +1000

    Added Base

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Test file</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
