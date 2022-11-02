2ac0f50c6cf917acc2edcb4a9cd6eaad442f52c4
David D <daviddawood6161@hotmail.com>
2022-11-02 21:36:58 +1000

    Fixed missing images on deployment

--- my-app/src/containers/projects/projects.tsx
+++ my-app/src/containers/projects/projects.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Project from "../../components/Project";
 import styles from "./Projects.module.scss";
+import Ecom1 from "../../images/E-Commerance/E-commerce-1.png";
+import Ecom2 from "../../images/E-Commerance/E-commerce-2.png";
+import Ecom3 from "../../images/E-Commerance/E-commerce-3.png";
+import Ecom4 from "../../images/E-Commerance/E-commerce-4.png";
+import calculator from "../../images/Calculator/calculator.png";
+import morseCode from "../../images/Morse-Code/morseCode.png";
 
 function Projects() {
     return (
@@ -8,9 +14,8 @@ function Projects() {
             <div>
                 <h2>Projects</h2>
                 <p>
-                    Some of my most noteworthy projects, be sure to take a look
-                    at my <a href="https://github.com/DavidDawood">Git</a> as
-                    many of the smaller projects are listed there
+                    Some of my most noteworthy projects, be sure to take a look at my{" "}
+                    <a href="https://github.com/DavidDawood">Git</a> as many of the smaller projects are listed there
                 </p>
             </div>
             <br />
@@ -18,29 +23,15 @@ function Projects() {
                 <Project
                     name={"E-commerce"}
                     link="https://github.com/DavidDawood/E-commerce"
-                    description={
-                        "A shopfront for space themed stationary, using javascript, HTML, SASS & Firebase"
-                    }
-                    previews={[
-                        "/images/E-Commerance/E-commerce-1.png",
-                        "/images/E-Commerance/E-commerce-2.png",
-                        "/images/E-Commerance/E-commerce-3.png",
-                        "/images/E-Commerance/E-commerce-4.png",
-                    ]}
-                    alt={[
-                        "Pront page",
-                        "Found items page",
-                        "Pen holder item",
-                        "Cart Page",
-                    ]}
+                    description={"A shopfront for space themed stationary, using javascript, HTML, SASS & Firebase"}
+                    previews={[Ecom1, Ecom2, Ecom3, Ecom4]}
+                    alt={["Pront page", "Found items page", "Pen holder item", "Cart Page"]}
                 />
                 <Project
                     name={"Calculator"}
                     link="https://github.com/DavidDawood/Project_Calculator"
-                    description={
-                        "A basic calculator using HTML, SCSS & Javascript"
-                    }
-                    previews={["/images/Calculator/calculator.png"]}
+                    description={"A basic calculator using HTML, SCSS & Javascript"}
+                    previews={[calculator]}
                     alt={["Calculator"]}
                 />
                 <Project
@@ -49,7 +40,7 @@ function Projects() {
                     description={
                         "Translate morse code easily with this online translator, comes with both an english to morse and a morse to english converter, made with HTML, SCSS & Javascript"
                     }
-                    previews={["/images/Morse-Code/morseCode.png"]}
+                    previews={[morseCode]}
                     alt={["Morse-Code Translator"]}
                 />
             </div>
